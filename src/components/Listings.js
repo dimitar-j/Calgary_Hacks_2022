ee105c1b08271a94b5c14f2578478e5b79a7d601
Dimitar Janevski <72237294+dimitar-j@users.noreply.github.com>
2022-02-19 23:24:56 -0700

    Added Listing cards

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import Listing from './Listing'
+import Grid from '@mui/material/Grid';
 
 var stylingObjects = {
     container: {
         paddingTop: "50px",
         width: "100%",
         height: "100%",
+        borderLeft: "1px solid",
+        paddingLeft: "50px"
     }
 }
 
@@ -15,6 +19,29 @@ export default class Listings extends Component {
                 <h1>
                     Recommended for you
                 </h1>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md = {5}>
+                        <Listing></Listing>
+                    </Grid>
+                    <Grid item xs={12} md = {5}>
+                        <Listing></Listing>
+                    </Grid>
+                    <Grid item xs={12} md = {5}>
+                        <Listing></Listing>
+                    </Grid>
+                    <Grid item xs={12} md = {5}>
+                        <Listing></Listing>
+                    </Grid>
+                    <Grid item xs={12} md = {5}>
+                        <Listing></Listing>
+                    </Grid>
+                    <Grid item xs={12} md = {5}>
+                        <Listing></Listing>
+                    </Grid>
+                    <Grid item xs={12} md = {5}>
+                        <Listing></Listing>
+                    </Grid>
+                </Grid>
             </div>
         )
     }
