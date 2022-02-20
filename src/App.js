6caa5b3913f94b96ff57d4e8f49afccdfaf82bbc
Dimitar Janevski <72237294+dimitar-j@users.noreply.github.com>
2022-02-19 22:00:53 -0700

    Implemented React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
 // Home Page
-import HomePage from './pages/Home.js';
-import Dashboard from './pages/Dashboard.js'
-
+import HomePage from "./pages/Home.js";
+/* import Login from "./pages/Login.js";
+import Dashboard from "./pages/Dashboard.js";
+import Register from "./pages/Register.js";
+import Tags from "./pages/Tags.js"; */
 
 function App() {
   return (
-    <div className="App">
-      <HomePage></HomePage>
-    </div>
+
+      <div className="App">
+        <Routes>
+          <Route path = "/" element = {<HomePage/>} exact/>
+{/*       <Route path = "/login" component = {<Login/>}/>
+          <Route path = "/dashboard" component = {<Dashboard/>}/>
+          <Route path = "/register" component = {<Register/>}/>
+          <Route path = "/tags" component = {<Tags/>}/> */}
+        </Routes>
+      </div>
+
   );
 }
 
