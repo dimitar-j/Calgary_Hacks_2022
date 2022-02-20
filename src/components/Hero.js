4bfc38b17bb49a69f9868bdc179a77d305ce7b84
Dimitar Janevski <72237294+dimitar-j@users.noreply.github.com>
2022-02-19 19:18:30 -0700

    Added "See what customers say message" to home page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,7 +9,7 @@ var stylingObject = {
         justifyContent: "space-evenly",
         alignItems: "center",
         paddingTop: "45px",
-        paddingBottom: "90px"
+        paddingBottom: "45px"
     },
     textContainer: {
         
@@ -25,15 +25,23 @@ var stylingObject = {
 
 function Hero() {
     return (
-        <div style = {stylingObject.container}>
-            <div style = {stylingObject.textContainer}>
-                <h1>Shop fresh home made food.</h1>
-                <p>Save Time. Save Money. Eat Well.</p>
+        <React.Fragment>
+            <div style = {stylingObject.container}>
+                <div style = {stylingObject.textContainer}>
+                    <h1>Shop fresh, home-made food.</h1>
+                    <p>Save Time. Save Money. Eat Well.</p>
+                </div>
+                <div style = {stylingObject.imageContainer}>
+                    <img src={require('../assets/ChefImage.png')} />
+                </div>
             </div>
-            <div style = {stylingObject.imageContainer}>
-                <img src={require('../assets/ChefImage.png')} />
+            <div style={{background:"#fffff3", textAlign:"center", fontSize:"26px", paddingBottom:"45px"}}>
+                See what our customers say
+                <br></br>
+                <br></br>
+                <img src={require('../assets/arrow.png')} width="30" height="30"/>
             </div>
-        </div>
+        </React.Fragment>
     )
 }
 
