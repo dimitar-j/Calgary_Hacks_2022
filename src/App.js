8b27c69d6a1e601551cd397a74f394fa16761b4a
Ajay Arumugam <ajayarumugam07@gmail.com>
2022-02-19 22:52:41 -0700

    firebase integrated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,32 @@ import {Routes, Route, BrowserRouter} from "react-router-dom";
 // Home Page
 import HomePage from "./pages/Home.js";
 import Dashboard from "./pages/Dashboard.js";
+import { db } from "./firebase-config";
+
+import { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+
 /* import Login from "./pages/Login.js";
 import Dashboard from "./pages/Dashboard.js";
 import Register from "./pages/Register.js";
 import Tags from "./pages/Tags.js"; */
 
 function App() {
+  const [businesses, setBusinesses] = useState([]);
+  const businessCollectionRef = collection(db, "business");
+
+  useEffect(() => {
+    
+    const getBusinesses = async () => {
+      const data = await getDocs(businessCollectionRef);
+      setBusinesses(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+      console.log(businesses);
+    }
+
+    getBusinesses();
+
+  }, []);
+
   return (
 
       <div className="App">
