d7305b3e3aea74526eaaa5021b811ef46454c07c
Dimitar Janevski <72237294+dimitar-j@users.noreply.github.com>
2022-02-19 23:30:10 -0700

    Fixed sizing of listings

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -4,11 +4,10 @@ import Grid from '@mui/material/Grid';
 
 var stylingObjects = {
     container: {
-        paddingTop: "50px",
-        width: "100%",
+        width: "90%",
         height: "100%",
         borderLeft: "1px solid",
-        paddingLeft: "50px"
+        padding: "50px"
     }
 }
 
@@ -20,25 +19,25 @@ export default class Listings extends Component {
                     Recommended for you
                 </h1>
                 <Grid container spacing={2}>
-                    <Grid item xs={12} md = {5}>
+                    <Grid item xs={11} md = {6}>
                         <Listing></Listing>
                     </Grid>
-                    <Grid item xs={12} md = {5}>
+                    <Grid item xs={11} md = {6}>
                         <Listing></Listing>
                     </Grid>
-                    <Grid item xs={12} md = {5}>
+                    <Grid item xs={11} md = {6}>
                         <Listing></Listing>
                     </Grid>
-                    <Grid item xs={12} md = {5}>
+                    <Grid item xs={11} md = {6}>
                         <Listing></Listing>
                     </Grid>
-                    <Grid item xs={12} md = {5}>
+                    <Grid item xs={11} md = {6}>
                         <Listing></Listing>
                     </Grid>
-                    <Grid item xs={12} md = {5}>
+                    <Grid item xs={11} md = {6}>
                         <Listing></Listing>
                     </Grid>
-                    <Grid item xs={12} md = {5}>
+                    <Grid item xs={11} md = {6}>
                         <Listing></Listing>
                     </Grid>
                 </Grid>
