28258a46be888831fd1d94b88dd132c1d0dafe5a
HeDoLe <47370336+HeDoLe@users.noreply.github.com>
2022-02-19 19:43:09 -0700

    Created routes
    The routes are creates for all pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
 // Home Page
 import HomePage from './pages/Home.js';
-
+import Home from './pages/Home.js'
+import Login from './pages/Login.js'
+import Dashboard from './pages/Dashboard.js'
+import Register from './pages/Register.js'
+import Tags from './pages/Tags.js'
 
 function App() {
   return (
-    <div className="App">
-      <HomePage></HomePage>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <HomePage></HomePage>
+      </div>
+      <switch>
+        <Route path = '/' component = {Home} exact/>
+        <Route path = '/login' component = {Login}/>
+        <Route path = '/dashboard' component = {Dashboard}/>
+        <Route path = '/register' component = {Register}/>
+        <Route path = '/tags' component = {Tags}/>
+      </switch>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
